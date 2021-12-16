20051771c56be5c73cbdc70276dbe74d9476be4e
tribit <tribit@mail.ru>
2021-12-16 17:35:51 +0300

    Create repository
    Push all files

---
+++ Assets/Script/EnemySpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public Transform Position;
+    public GameObject EnemyPrefab;
+
+    void Start()
+    {
+        var e01 = Instantiate(EnemyPrefab, Position.position, Quaternion.identity);
+    }
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    private Vector3 _direction;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _direction.x = Input.GetAxis("Horizontal");
+        _direction.z = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = transform.position + _direction * Time.fixedDeltaTime * Speed;
+    }
+}
